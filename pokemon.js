f34a3da1a11239aefe68357208f5615bb008b994
Juan Diego Carmona Arredondo <104167947+DiegoJCA125@users.noreply.github.com>
2022-09-25 07:32:14 -0500

    Add files via upload

--- pokemon.js
+++ pokemon.js
@@ -29,7 +29,7 @@ function seleccionarMascotaJugador(){
     sectionSeleccionarMascota.style.display = 'none'
     
     let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
-    sectionSeleccionarAtaque.style.display = 'block'
+    sectionSeleccionarAtaque.style.display = 'flex'
 
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
@@ -121,28 +121,32 @@ function combate(){
 
 function revisarVidas(){
     if(vidasEnemigo == 0){
-        crearMensajeFinal("FELICITACIONES GANASTE LA PARTIDA 🥳")
+        crearMensajeFinal(" FELICITACIONES, GANASTE LA PARTIDA 🥳")
     } else if (vidasJugador == 0){
-        crearMensajeFinal("LO SENTIMOS, PERDISTE LA PARTIDA 😥")
+        crearMensajeFinal(" LO SENTIMOS, PERDISTE LA PARTIDA 😥")
     }
 }
 
 function crearMensaje(resultado) {
-    let sectionMensajes = document.getElementById('mensajes')
+    let sectionMensajes = document.getElementById('resultado')
+    let ataquesDelJugador = document.getElementById('ataques-del-jugador')
+    let ataquesDelEnemigo = document.getElementById('ataques-del-enemigo')
 
-    let parrafo = document.createElement('p')
-    parrafo.innerHTML = 'Tu mascota atacó con ' + ataqueJugador + ', la mascota del enemigo atacó con ' + ataqueEnemigo + '-' + resultado
+    let nuevoAtaqueDelJugador = document.createElement('p')
+    let nuevoAtaqueDelEnemigo = document.createElement('p')
 
-    sectionMensajes.appendChild(parrafo)
+    sectionMensajes.innerHTML = resultado
+    nuevoAtaqueDelJugador.innerHTML = ataqueJugador
+    nuevoAtaqueDelEnemigo.innerHTML = ataqueEnemigo
+
+    ataquesDelJugador.appendChild(nuevoAtaqueDelJugador)
+    ataquesDelEnemigo.appendChild(nuevoAtaqueDelEnemigo)
 }
 
 function crearMensajeFinal(resultadoFinal) {
-    let sectionMensajes = document.getElementById('mensajes')
+    let sectionMensajes = document.getElementById('resultado')
 
-    let parrafo = document.createElement('p')
-    parrafo.innerHTML = resultadoFinal
-
-    sectionMensajes.appendChild(parrafo)
+    sectionMensajes.innerHTML = resultadoFinal
 
     let botonFuego = document.getElementById('boton-fuego')
     botonFuego.disabled = true
@@ -152,7 +156,7 @@ function crearMensajeFinal(resultadoFinal) {
     botonTierra.disabled = true
 
     let sectionReiniciar = document.getElementById('reiniciar')
-    sectionReiniciar.style.display = 'block'
+    sectionReiniciar.style.display = 'flex'
 }
 
 function reiniciarJuego(){
